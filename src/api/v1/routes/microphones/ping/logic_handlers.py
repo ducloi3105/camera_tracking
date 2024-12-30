42b7cb45a854c31bbd190814421358469ad13105
loind <loind@airasia.com>
2024-12-30 13:29:32 +0700

    update api check

--- src/api/v1/routes/microphones/ping/logic_handlers.py
+++ src/api/v1/routes/microphones/ping/logic_handlers.py
@@ -1,21 +1,31 @@
 from src.bases.api.routes import RouteLogicHandler
 from src.clients.dcerno import DcernoClient
+from src.clients.vhd import VHDClient
 from src.bases.error.client import ClientError
-from config import DCERNO_CONFIG
+from config import DCERNO_CONFIG, VHD_CONFIG
 
 
 class MicrophonesPingLogicHandler(RouteLogicHandler):
     def run(self):
-        ping = False
-        client = DcernoClient(
-            host=DCERNO_CONFIG['host'],
-            port=DCERNO_CONFIG['port'],
-            timeout=2
-        )
+        mic_ping = False
         try:
+            client = DcernoClient(
+                host=DCERNO_CONFIG['host'],
+                port=DCERNO_CONFIG['port'],
+                timeout=2
+            )
             if client.get_all_units():
-                ping = True
+                mic_ping = True
         except ClientError as e:
-            print(e)
-
-        return dict(success=ping)
+            pass
+        cam_ping = False
+        try:
+            client = VHDClient(
+                uri=VHD_CONFIG['uri'],
+                logger=self.logger
+            )
+            res = client.ping()
+            cam_ping = True
+        except Exception as e:
+            pass
+        return dict(mic_ping=mic_ping, cam_ping=cam_ping)
