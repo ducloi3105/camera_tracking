d07505449d6f54ea63d079dd8a4accf139a2af8f
loind <loind@airasia.com>
2024-12-30 11:48:22 +0700

    update api ping mic

--- src/api/v1/routes/microphones/ping/logic_handlers.py
+++ src/api/v1/routes/microphones/ping/logic_handlers.py
@@ -13,7 +13,7 @@ class MicrophonesPingLogicHandler(RouteLogicHandler):
             timeout=2
         )
         try:
-            if client.socket_manager.socket:
+            if client.get_all_units():
                 ping = True
         except ClientError as e:
             print(e)
